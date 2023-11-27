964e00e2464f2418338f65d1cc43302dfa05831b
jaques <jaques.oliveira@gmail.com>
2023-11-27 20:40:21 -0300

    projeto

---
+++ src/main/java/br/com/jqs/aton/AtonApplication.java
@@ -0,0 +1,13 @@
+package br.com.jqs.aton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jqs/aton/AtonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jqs.aton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
